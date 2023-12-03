9c40cc4dc61a042a1b6a6b58e7cea26b881fe9ed
Okko <okko.contact@proton.me>
2023-12-03 16:16:44 +0200

    Day 2 - part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
+use std::fs::read_to_string;
+use std::rc::Rc;
 
-fn part1(input: String) -> u32 {
+fn part1(input: Rc<String>) -> u32 {
     let mut max_map: HashMap<&str, u32> = HashMap::new();
     max_map.insert("green", 13);
     max_map.insert("blue", 14);
@@ -23,7 +25,42 @@ fn part1(input: String) -> u32 {
     pass
 }
 
+fn part2(input: std::rc::Rc<String>) -> u32 {
+    let mut nums = vec![];
+    for line in input.lines() {
+        let mut greens = vec![];
+        let mut blues = vec![];
+        let mut reds = vec![];
+        for rounds in line.split(":").last().unwrap().split(";") {
+            for parts in rounds.split(",") {
+                let mut splt = parts.split(" ").skip(1);
+                let num = splt.next().unwrap().trim().parse::<u32>().unwrap();
+                let colour = splt.next().unwrap();
+                match colour {
+                    "green" => {
+                        greens.push(num);
+                    }
+                    "red" => {
+                        reds.push(num);
+                    }
+                    "blue" => {
+                        blues.push(num);
+                    }
+                    _ => unreachable!(),
+                }
+            }
+        }
+        nums.push(
+            greens.iter().max().unwrap() * blues.iter().max().unwrap() * reds.iter().max().unwrap(),
+        )
+    }
+    nums.iter().sum()
+}
+
 fn main() {
-    let input = std::fs::read_to_string("./inputs/02.in").unwrap();
-    println!("p1: {}", part1(input));
+    let input = read_to_string("./inputs/02.in").unwrap();
+    let input = Rc::new(input);
+    let p1 = part1(Rc::clone(&input));
+    let p2 = part2(Rc::clone(&input));
+    println!("p1: {p1}, p2: {p2}");
 }
