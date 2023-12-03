eb779d0bca29920f642a621668de543c550baeaf
Okko <okko.contact@proton.me>
2023-12-03 16:33:46 +0200

    start day 3..

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -2,11 +2,13 @@ use std::fs::read_to_string;
 use std::rc::Rc;
 
 fn part1(input: Rc<String>) -> u32 {
-    1
+    let valid_nums = vec![];
+    for line in input.lines() {}
+    valid_nums.iter().sum()
 }
 
 fn main() {
-    let input = Rc::new(read_to_string("./inputs/03.in")).unwrap();
+    let input = Rc::new(read_to_string("./inputs/03.in").unwrap());
     let p1 = part1(Rc::clone(&input));
 
     println!("{p1}");
