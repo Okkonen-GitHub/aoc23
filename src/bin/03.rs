d6b3241b8de1882b1d3527897e7c1d278b8f57b4
Okko <okko.contact@proton.me>
2023-12-04 21:11:12 +0200

    Work on Day 3 some more

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -4,10 +4,11 @@ use std::rc::Rc;
 const VALID_NUMS: [char; 10] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const NON_SYMBOL: [char; 11] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 
-// returns starting and ending indexes for a line
-fn find_num(line: &str) -> Vec<(usize, usize)> {
+// returns starting and ending indexes for a line and parses the number
+fn find_num(line: &str) -> Vec<(usize, usize, u32)> {
     let mut indexes = vec![];
     let mut i = 0;
+    let original = line;
     let mut line = line.chars();
     loop {
         let ch = match line.next() {
@@ -20,7 +21,6 @@ fn find_num(line: &str) -> Vec<(usize, usize)> {
             loop {
                 if let Some(next) = line.next() {
                     if VALID_NUMS.contains(&next) {
-                        // dbg!(next, start, end, i);
                         i += 1;
                         end += 1;
                     } else {
@@ -29,7 +29,8 @@ fn find_num(line: &str) -> Vec<(usize, usize)> {
                     }
                 }
             }
-            indexes.push((start, end));
+            let num = original.get(start..end + 1).unwrap().parse().unwrap();
+            indexes.push((start, end, num));
         }
         i += 1;
     }
@@ -38,10 +39,18 @@ fn find_num(line: &str) -> Vec<(usize, usize)> {
 
 fn part1(input: Rc<String>) -> u32 {
     let valid_nums = vec![];
-    for line in input.lines().take(1) {
+    let lines = input.lines();
+    for (i, line) in lines.clone().enumerate().take(2) {
         let indexes = find_num(line);
-        for (start, end) in indexes {
-            let diff = end - start;
+        for (start, end, num) in indexes {
+            if i > 0 {
+                let over = if let Some(line_above) = lines.clone().nth(i - 1) {
+                    line_above.get(start - 1..end + 2)
+                } else {
+                    None
+                };
+                dbg!(over, num);
+            }
         }
     }
     valid_nums.iter().sum()
